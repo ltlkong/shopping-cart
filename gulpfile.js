9f986698a6039f3f62e49e4e15ac058fa4f9661e
Tielin Li <tielinlilili84@student.mitt.ca>
2020-12-04 15:52:11 -0600

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,29 @@
 const { src, dest, parallel} = require("gulp");
 const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const srcmaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+
+function cssTask() {
+  return src("src/css/*")
+  .pipe(srcmaps.init())
+  .pipe(concat("all.css"))
+  .pipe(postcss([cssnano()]))
+  .pipe(srcmaps.write())
+  .pipe(dest("dist/css"));
+}
+
+function htmlTask() {
+  return src("src/*.html").pipe("dist/html");
+}
+
+function jsTask() {
+  return src("src/js/*").pipe("dist/html");
+}
+
+function imagesTask() {
+  return src("src/images/*").pipe(imagemin()).pipe(dest("dist/images"))
+}
+
+exports.default = parallel(cssTask,jsTask,htmlTask,imagesTask); 
